9effaf778c714c168ab4f74e697e94f3e96e40c1
zakya sharmeen <sakarim9124@gmail.com>
2025-03-10 21:29:35 +0530

    updated vercel-ui

--- backend/app.js
+++ backend/app.js
@@ -10,14 +10,28 @@ PORT = 8000
 
 const app = express()
 // app.use(cors())
-const corsOptions = {
-    origin: "http://localhost:3000",
-    credentials: true, 
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
-};
+// const corsOptions = {
+//     origin: "http://localhost:3000",
+//     credentials: true, 
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     allowedHeaders: "Content-Type,Authorization",
+// };
+// app.use(
+//     cors({
+//       origin: "http://localhost:3000", 
+//       credentials: true, 
+//     })
+//   );
+app.use(
+    cors({
+      origin: "https://vercel-p2-restaurant-app.vercel.app", 
+      credentials: true,
+    })
+  );
+  
+  app.use(express.json());
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(express.json())
 
 
